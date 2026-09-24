2b794b91bd9fd7d1fc6f20c8104bdf591cc5d4f6
alirasim47 <spiderat2019@gmail.com>
2026-09-24 19:52:46 +0300

    Update layout.jsx

--- alnather-clinic/alnather/app/layout.jsx
+++ alnather-clinic/alnather/app/layout.jsx
@@ -9,6 +9,7 @@ export default function RootLayout({ children }) {
   return (
     <html lang="ar" dir="rtl">
       <head>
+        <meta name="viewport" content="width=device-width, initial-scale=1, maximum-scale=1" />
         <link rel="preconnect" href="https://fonts.googleapis.com" />
         <link rel="preconnect" href="https://fonts.gstatic.com" crossOrigin="" />
         <link
